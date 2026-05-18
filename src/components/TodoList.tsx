9cf1a40d7a6f885c1561c1be6e75d56360b306b5
TavionK <tavion.k.britt99@gmail.com>
2026-05-18 01:17:18 -0400

    Refactor TodoList button styles: improve font weight, color contrast, and text transformation for better consistency

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,7 +25,7 @@ export default function TodoList({ tasks, setTask }: TodoListProps) {
         </p>
         <button
           onClick={handleClearAll}
-          className="cursor-pointer underline hover:text-purple-700 hover:decoration-purple-700 transition-colors duration-300 ease-in-out a11y-rings focus-visible:no-underline rounded-xs"
+          className="cursor-pointer underline font-semibold text-purple-800  a11y-rings focus-visible:no-underline uppercase"
         >
           Clear All
         </button>

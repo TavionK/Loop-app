b7b62a6be6a7c8f3f4686c13cb762d4c0dc16159
TavionK <tavion.k.britt99@gmail.com>
2026-05-18 12:57:04 -0400

    Refactor Auth component: remove view state and associated components for simplified authentication flow

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,14 +1,8 @@
 import { useState } from "react";
 import { supabase } from "../supabaseClient.ts";
-import Login from "./Login.tsx";
-import Signup from "./Signup.tsx";
 import { FcGoogle } from "react-icons/fc";
-import ResetPassword from "./ResetPassword.tsx";
 
 export default function Auth() {
-  const [view, setView] = useState<"login" | "signup" | "reset-password">(
-    "login",
-  );
   const [error, setError] = useState<string | null>(null);
 
   const handleGoogleLogin = async () => {
@@ -24,10 +18,6 @@ export default function Auth() {
   return (
     <div className="flex flex-col justify-between grow">
       <div>
-        {view === "login" && <Login setView={setView} />}
-        {view === "signup" && <Signup />}
-        {view === "reset-password" && <ResetPassword />}
-
         <div className="relative flex items-center my-6">
           <div className="grow border-t border-gray-300"></div>
           <span className="mx-4 text-sm text-gray-500">OR</span>
@@ -44,31 +34,6 @@ export default function Auth() {
 
         {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
       </div>
-      {/*Toggle between login and signup*/}
-      <p className="text-center">
-        {view === "login" && (
-          <>
-            Don't have an account?{" "}
-            <span
-              onClick={() => setView("signup")}
-              className="cursor-pointer text-purple-800 font-semibold underline"
-            >
-              Sign up
-            </span>
-          </>
-        )}
-        {view === "signup" && (
-          <>
-            Already have an account?{" "}
-            <span
-              onClick={() => setView("login")}
-              className="cursor-pointer text-purple-800 font-semibold underline"
-            >
-              Login
-            </span>
-          </>
-        )}
-      </p>
     </div>
   );
 }
